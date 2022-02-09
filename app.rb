c3dea44fdc795a840e3459fb7a8afd33e6d49f46
Alfred-Lindell-nti-johanneberg <74763165+Alfred-Lindell-nti-johanneberg@users.noreply.github.com>
2022-02-09 16:37:48 +0100

    uppvisning av posts

--- app.rb
+++ app.rb
@@ -9,3 +9,11 @@ get('/') do
     slim(:firstpage)
 end
 
+get('/pictures') do
+    db = SQLite3::Database.new("db/Birdwatch_forum.db")
+    db.results_as_hash = true
+    posts = db.execute("SELECT * FROM posts WHERE topic_id = 1")
+    puts posts
+    slim(:pictures, locals:{posts:posts})
+
+end
