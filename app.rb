086216b853daceda9a1249f05ca7f84bc63d8b45
Alfred-Lindell-nti-johanneberg <74763165+Alfred-Lindell-nti-johanneberg@users.noreply.github.com>
2022-02-23 16:44:19 +0100

    man kan nu lägga till posts och även ta bort dom.

--- app.rb
+++ app.rb
@@ -17,3 +17,38 @@ get('/pictures') do
     slim(:pictures, locals:{posts:posts})
 
 end
+
+get('/addpost') do
+    slim(:addPost)
+end
+
+post('/postmade') do
+    @id = 1 #add session för inloggad user id
+    title = params[:title]
+    body=params[:body]
+    if params[:picture] == "on"
+        topic = 1
+    elsif params[:spots] == "on"
+        topic = 2
+    elsif params[:bogos] == "on"
+        topic = 3
+    else 
+        topic = 0
+    end
+    db = SQLite3::Database.new("db/Birdwatch_forum.db")
+    db.execute("INSERT INTO posts (title, body, user_id, topic_id) VALUES (?, ?, ?, ?)",title,body,@id,topic)
+    redirect("/addpost")    
+end
+
+get('/deletePost') do
+    slim(:deletePost)
+
+end
+
+post('/postdeleted') do
+    db = SQLite3::Database.new("db/Birdwatch_forum.db")
+    title = params[:title]
+    db.execute("DELETE FROM posts WHERE title = ?",title)
+
+    redirect("/deletePost")
+end
