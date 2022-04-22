e9c06ee9823714e17bcfe5baf8084098a392faec
Alfred-Lindell-nti-johanneberg <74763165+Alfred-Lindell-nti-johanneberg@users.noreply.github.com>
2022-04-23 00:05:46 +0200

    login, register, username på posts, text i landingpage, logutfunktion.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ enable :sessions
 
 
 get('/') do
-    session[:id]=1
+
     slim(:firstpage)
 end
 
@@ -15,8 +15,26 @@ get('/pictures') do
     db = SQLite3::Database.new("db/Birdwatch_forum.db")
     db.results_as_hash = true
     posts = db.execute("SELECT * FROM posts WHERE topic_id = 1")
+    users = db.execute("SELECT * FROM users")
     puts posts
-    slim(:pictures, locals:{posts:posts})
+    puts users
+    slim(:pictures, locals:{posts:posts,users:users})
+
+end
+get('/spots') do
+    db = SQLite3::Database.new("db/Birdwatch_forum.db")
+    db.results_as_hash = true
+    posts = db.execute("SELECT * FROM posts WHERE topic_id = 2")
+    users = db.execute("SELECT * FROM users")
+    slim(:spots, locals:{posts:posts,users:users})
+
+end
+get('/bogos') do
+    db = SQLite3::Database.new("db/Birdwatch_forum.db")
+    db.results_as_hash = true
+    posts = db.execute("SELECT * FROM posts WHERE topic_id = 3")
+    users = db.execute("SELECT * FROM users")
+    slim(:bogos, locals:{posts:posts,users:users})
 
 end
 
@@ -63,11 +81,37 @@ post('/users/new') do
   
     if (password == password_confirm)
       password_digest = BCrypt::Password.create(password)
-      db = SQLite3::Database.new('db/todo_login_sida.db')
-      db.execute("INSERT INTO users (username,pwdigest,email) VALUES (?,?)",username,password_digest,email)
+      db = SQLite3::Database.new("db/Birdwatch_forum.db")
+      db.execute("INSERT INTO users (name,role_id,pwdigest,email) VALUES (?,?,?,?)",username,nil,password_digest,email)
       redirect('/')
     else
       #felhantering
       "Lösenorden matchade inte"
     end
+end
+
+get('/logout') do
+    session.destroy
+    redirect('/')
+end
+
+get('/login') do
+    slim(:'users/login')
+end
+
+post('users/login') do
+    username = params[:username]
+  password = params[:password]
+  db = SQLite3::Database.new('db/Birdwatch_forum.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE name = ?",username).first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id]= id
+    redirect('/')
+  else
+    "Fel lösen, din skojare"
+  end
 end
