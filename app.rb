7668af971d26845091fd9a41b99834e966f887b3
Alfred-Lindell-nti-johanneberg <74763165+Alfred-Lindell-nti-johanneberg@users.noreply.github.com>
2022-05-08 01:50:36 +0200

    Lite valideringar, mycket Yardoc

--- app.rb
+++ app.rb
@@ -6,118 +6,300 @@ require_relative 'model.rb'
 
 enable :sessions
 
+include Model
 
-
-
-
-
+# 
+# display landing page
+# 
 get('/') do
   slim(:index)
 end
 
+# 
+# Displays posts with the topic pictures
+# 
+# @see Model#show_posts_topic
 get('/pictures') do
     keys=show_posts_topic(1,session[:id])
     slim(:'posts/index_pictures', locals:{posts:keys[0],users:keys[1], current_user:keys[2]})
-
 end
+
+# 
+# Displays posts with the topic birdwatching spots
+#
+# @see Model#show_posts_topic
 get('/spots') do
     keys=show_posts_topic(2,session[:id])
     slim(:'posts/index_spots', locals:{posts:keys[0],users:keys[1], current_user:keys[2]})
-
 end
+
+# 
+# Displays posts with the topic bogos binted
+#
+# @see Model#show_posts_topic
 get('/bogos') do
     keys=show_posts_topic(3,session[:id])
     slim(:'posts/index_bogos', locals:{posts:keys[0],users:keys[1], current_user:keys[2]})
 end
 
+# 
+# Displays a form where you can create a new post
+#
 get('/posts/new') do
     slim(:'posts/new')
 end
 
+# 
+# Displays the post with this id
+# 
+# @param [integer] id The id of post shown
+# 
+# @see Model#show_post
 get('/posts/:id') do
     id = params[:id]
     keys = show_post(id)
     slim(:'posts/show', locals:{post:keys[0],user:keys[1],tags:keys[2]})
 end
 
+# 
+# Checks if user is logged in and if all fields have been filled in, then creates a post
+# 
+# @param [string] title The title of new post
+# @param [string] body The bodytext of new post
+# @param [integer] topic The topic the post will be in
+# @param [nil/array<Hash>] file The information of attatched img file, or nil if no file has been attatched
+# 
+# @see Model#empty_string?
+# @see Model#create_post
 post('/posts/create') do
     id = session[:id] 
     title = params[:title]
     body=params[:body]
     topic = params[:topic].to_i
     file = params[:file]
-    create_post(id,title,body,topic,file)
-    redirect("/posts/new")    
+    if session[:id]==nil
+        redirect('/posts/new')
+    end
+    if empty_string?(title) || empty_string?(body)
+        session[:create_post_error] = "One or more of your fields are empty, please fill them in"
+        redirect("/posts/new")
+    else
+        session[:create_post_error] = ""
+        create_post(id,title,body,topic,file)
+        redirect("/posts/#{id}")   
+    end 
 end
 
+# 
+# Checks if the user owns the post and then adds tag to/removes tag from post 
+# 
+# @param [integer] post_id The ID of post
+# @param [string] post_tag The tag that will be added to/removed from post
+# 
+# @see Model#user_id_by_post_id
+# @see Model#add_remove_tags
 post('/post/:id/tags') do
     post_id = params[:id]
     post_tag = params[:tag]
-    add_remove_tags(post_id,post_tag)
-    redirect("/posts/#{post_id}")
+    if user_id_by_post_id(post_id) == session[:id]
+        add_remove_tags(post_id,post_tag)
+        redirect("/posts/#{post_id}")
+    end
 end
 
+# 
+# Updates the title and body of a post if session id == the posts user_id
+# 
+# @param [integer] id The id of post
+# @param [string] title The new title to post
+# @param [string] body The new textbody to post
+# 
+# @see Model#update_post
+# @see Model#user_id_by_post_id
 post('/post/:id/update') do
     post_id = params[:id]
     title = params[:title]
     body = params[:body]
-    update_post(post_id,title,body)
-    redirect("posts/#{post_id}")
+    if user_id_by_post_id(post_id) == session[:id]
+        update_post(post_id,title,body)
+        redirect("posts/#{post_id}")
+    end
 end
 
+# 
+# Checks if the user owns the post or if they are an admin, only then does it delete the post with id = id
+# 
+# @param [integer] id The id of post
+# 
+# @see Model#is_admin?
+# @see Model#delete_post
 post('/post/:id/delete') do
     id = params[:id]
-    delete_post(id)
+    if session[:id] !=nil
+        if id == session[:id] || is_admin?(session[:id])
+            delete_post(id)
+        end
+    end
+    redirect("/users/#{user_id}")
 end
 
-
+# 
+# Displays a form where you can create a new post
+# 
 get('/users/new') do  
     slim(:'users/new')
 end
 
+# 
+# Creates a new user with information from parasm inserted to db if no fields were empty and if username does not already exist
+# 
+# @param [string] username Name of new user
+# @param [string] email The email of new user
+# @param [string] password The password of new user
+# @param [string] password_confirm A string used to check user can write the same password twice in a row
+# 
+# @see Model#empty_string?
+# @see Model#username_exist?
+# @see Model#new_user
 post('/users') do
     username = params[:username]
     email = params[:email]
     password = params[:password]
     password_confirm = params[:password_confirm]
     session[:regattempt]=0
+    if empty_string?(username) || empty_string?(email) || empty_string?(password) || empty_string?(password_confirm)
+        session[:regattempt_error] = "Lämna inget fält tomt"
+        session[:regattempt]+=1
+        redirect('/users/new')
+    else
+        session[:regattempt_error] = ""
+    end
+    if username_exist?(username)
+        session[:regattempt_error] = "Username already exists, please try a different username."
+        session[:regattempt]+=1
+        redirect('/users/new')
+    else
+        session[:regattempt_error] = ""
+    end
+
     session[:regattempt] = new_user(username,email,password,password_confirm)
     if session[:regattempt]== nil
         redirect('/')
     else
-        redirect('/register')
+        redirect('/users/new')
     end
     
 end
 
+# 
+# Destroys session, effectiely logging out of account
+#
 get('/logout') do
     session.destroy
     redirect('/')
 end
 
+# 
+# Displays a form where you can login to an existing account
+# 
 get('/user/login') do
     slim(:'users/login')
 end
 
+# 
+# Shows the page of a user alongside all of their posts, forms to delete posts and forms to update password and email
+# 
+# @param [integer] id The ID of The user whose page is being shown
+# 
+# @see Model#show_user
 get('/users/:id') do
-    id = session[:id]
-    posts =show_user(id)
-    slim(:'users/show', locals:{posts:posts})
+    user_page_id = params[:id]
+    posts =show_user(user_page_id)
+    slim(:'users/show', locals:{posts:posts,user_page_id:user_page_id})
 end
 
+# 
+# Updates the email of a user
+# 
+# @param [string] email New email of user
+# 
+# @see Model#update_user_email
 post('/users/:id/update/email') do
-    email = params[:email]
-    update_user_email(email)
+    email = params[:email]   
+    session[:email] = update_user_email(email,session[:id])
+    redirect("/users/#{session[:id]}")
 end
+
+# 
+# Updates password of user if the user being updated is same as the one logged in, the old password is the correct password and if new pw is same as new pw_confirm
+# 
+# @param [integer] user_page_id The ID of the user whose page you are on
+# @param [string] old_password The old password of user
+# @param [string] new_password the new password of user
+# @param [string] new_password_confirm New password confirm
+# 
+# @see Model#select_user_by_name
+# @see Model#correct_password_to_user
+# @see Model#update_user_pw
 post('/users/:id/update/password') do
+    user_page_id = params[:id]
     old_password=params[:password_old]
     new_password=params[:password]
     new_password_confirm=params[:password_confirm]
-    update_user_pw(old_password,new_password,new_password_confirm)
+    session[:change_pw_err] = ""
+    user = select_user_by_name(session[:username])
+    if user_page_id==user["id"] || correct_password_to_user(old_password,user)
+        if new_password==new_password_confirm
+            update_user_pw(new_password,user["id"])
+        else
+            session[:change_pw_err] = "Dina nya lösenord matchade inte"
+        end
+    else
+        session[:change_pw_err] = "Fel lösen"
+
+    end
+    redirect("/users/#{session[:id]}")
 end
 
+# 
+# Loggs in to existing user, prevents brute force hacking, checks for empty fields, Checks if user exists and checks if password is correct
+# 
+# @param [string] username The name of user that wants to log in
+# @param [sting] password The Password of user that wants to log in
+# 
+# @see Model#empty_string?
+# @see Model#select_user_by_name
+# @see Model#correct_password_to_user
 post('/login') do
     username = params[:username]
     password = params[:password]
-    login(username,password)
+    
+    session[:loginattempt]=0 if Time.new.to_i - session[:last_attempt].to_i > 300
+    if session[:loginattempt] > 4
+        session[:signinerror] = 'För många misslyckade försök. Var vänlig och försök igen senare'
+        redirect('/user/login') 
+    end
+    if empty_string?(username) || empty_string?(password)
+        session[:signinerror] = "Lämna inget fält tomt"
+        session[:loginattempt]+=1
+        redirect('/users/new')
+    end
+    user = select_user_by_name(username)
+    if user==nil 
+        session[:loginattempt] +=1
+        session[:last_attempt] = Time.now
+        redirect('/user/login')
+    end 
+    if correct_password_to_user(password,user)
+        session[:id]= user["id"]
+        session[:username] = username
+        session[:email] = user["email"]
+        session[:loginattempt] = nil
+        session[:signinerror] = ""
+        return redirect('/')
+    else
+        session[:loginattempt] +=1
+        session[:last_attempt] = Time.now
+        return redirect('/user/login')
+    end
 end
--- model.rb
+++ model.rb
@@ -1,191 +1,286 @@
-#
-# connects to and database saves database to a variable 'db' 
-#
-# @return [array<Hash>] returns db
-#
-def connect_to_db()
-    db = SQLite3::Database.new("db/Birdwatch_forum.db")
-end
-
-
-#
-# <Description>
-#
-# @param [<Type>] topic_id <description>
-# @param [<Type>] user_id <description>
-#
-# @return [<Type>] <description>
-#
-def show_posts_topic(topic_id,user_id)
-    db = connect_to_db()
-    db.results_as_hash = true
-    posts = db.execute("SELECT * FROM posts WHERE topic_id = ?",topic_id)
-    users = db.execute("SELECT * FROM users")
-    if user_id == nil
-        current_user = nil
-    else
-        current_user= db.execute("SELECT role_id FROM users WHERE id = ?",session[:id]).first
+module Model
+    #
+    # connects to and database saves database to a variable 'db' 
+    #
+    # @return [array<Hash>] returns db
+    #
+    def connect_to_db()
+        db = SQLite3::Database.new("db/Birdwatch_forum.db")
     end
-    return [posts,users,current_user]
-end
 
-#
-# <Description>
-#
-# @param [<Type>] id <description>
-#
-# @return [<Type>] <description>
-#
-def show_post(id)
-    db = connect_to_db()
-    db.results_as_hash = true
-    post = db.execute("SELECT * FROM posts WHERE id = ?",id).first
-    user = db.execute("SELECT * FROM posts INNER JOIN users ON users.id = posts.user_id WHERE posts.id = ? ",id).first
-    tags = db.execute("SELECT * FROM tags INNER JOIN posts_tags_rel ON tags.id = posts_tags_rel.tags_id WHERE posts_tags_rel.posts_id = ?",id)
-    return [post,user,tags]
-end
 
-def create_post(id,title,body,topic,file)
-    if file && file[:filename]
-        path = File.join("./public/img/",file[:filename])
-        path_for_db = File.join("img/",file[:filename])
-        tempfile=file[:tempfile]
-        File.open(path, 'wb') do |f|
-            f.write(tempfile.read)
-        end
-    else
-        path_for_db = nil
-    end
-    db = connect_to_db()
-    db.execute("INSERT INTO posts (title, body, user_id, topic_id,img_path) VALUES (?, ?, ?, ?,?)",title,body,id,topic,path_for_db)
-end
-
-def update_post(post_id,title,body)
-    db=connect_to_db()
-    db.execute("UPDATE posts SET (title,body) = (?,?) WHERE id=? ",title,body,post_id)
-end
-
-def add_remove_tags(post_id,post_tag)
-    db=connect_to_db()
-    db.results_as_hash=true
-    tags = db.execute("SELECt * FROM tags")
-    #kolla om given tag redan finns i tags table och lägg då till den
-    @tag_exists = false
-    tags.each do |tag|
-        p tag["name"]
-        if tag["name"]==post_tag
-            @tag_exists=true
-        end
-    end
-    if @tag_exists == false 
-        db.execute("INSERT INTO tags (name) VALUES (?)",post_tag)
-    end
-    # kolla om taggen finns på post:en eller inte
-    post_tag_id = db.execute("SELECT id FROM tags WHERE name=?",post_tag).first["id"] # name är unikt så det finns bara en av alla tags
-    if db.execute("SELECT * FROM posts_tags_rel WHERE (posts_id,tags_id) = (?,?)",post_id,post_tag_id).first == nil
-        db.execute("INSERT INTO posts_tags_rel (posts_id,tags_id) VALUES (?,?)",post_id,post_tag_id)
-    else #finns det redan taggen på posten så tas relationen bort från posts_tags_rel
-        db.execute("DELETE FROM posts_tags_rel WHERE (posts_id,tags_id) = (?,?)",post_id,post_tag_id)
-    end
-end
-
-def delete_post(post_id,user_id)
-    db = connect_to_db()
-    post_user_id = db.execute("SELECT user_id FROM posts where id = ?",post_id)
-    path = db.execute("SELECT img_path FROM posts where id = ?", post_id).first
-    p path
-    if post_user_id != []
-        user_role=db.execute("SELECT role_id FROM users where id=?",session[:id]).first
-        if user_id==post_user_id[0][0] || user_role[0] == 1
-            db.execute("DELETE FROM posts WHERE id = ?",post_id)
-            db.execute("DELETE FROM posts_tags_rel WHERE posts_id = ?",post_id)
-            if path[0]!=nil
-                File.delete("./public/#{path[0]}") if File.exist?("./public/#{path[0]}")
-            end
-        end      
-    end
-    return redirect("/users/#{user_id}")
-end
-
-def new_user(username,email,password,password_confirm)
-    
-    regattempt=0
-
-    if (password == password_confirm)
-      password_digest = BCrypt::Password.create(password)
-      db = connect_to_db()
-      db.execute("INSERT INTO users (name,role_id,pwdigest,email) VALUES (?,?,?,?)",username,0,password_digest,email)
-      regattempt = nil
-      return regattempt
-    else
-      regattempt +=1
-      return regattempt
-    end
-end
-
-def login(username,password)
-    db = connect_to_db()
-    db.results_as_hash = true
-    session[:loginattempt]=0 if Time.new.to_i - session[:last_attempt].to_i > 300
-    if session[:loginattempt] > 4
-        session[:signinerror] = 'För många misslyckade försök. Var vänlig och försök igen senare'
-        return redirect('/user/login')
-    end
-    result = db.execute("SELECT * FROM users WHERE name = ?",username).first
-    if result==nil 
-        session[:loginattempt] +=1
-        session[:last_attempt] = Time.now
-        return redirect('/user/login')
-    end  
-    pwdigest = result["pwdigest"]
-    id = result["id"]
-
-    if BCrypt::Password.new(pwdigest) == password
-        session[:id]= id
-        session[:username] = username
-        session[:email] = result["email"]
-        session[:loginattempt] = nil
-        session[:signinerror] = ""
-        return redirect('/')
-    else
-        session[:loginattempt] +=1
-        session[:last_attempt] = Time.now
-        return redirect('/user/login')
-    end
-end
-
-def update_user_pw(old_password,new_password,new_password_confirm)
-    db = connect_to_db()
-    db.results_as_hash=true
-    session[:regattempt]=0
-    session[:change_pw_err] = ""
-    pwdigest_old = db.execute("SELECT * FROM users WHERE id = ?",session[:id]).first["pwdigest"]
-    if BCrypt::Password.new(pwdigest_old) == old_password
-        if (new_password == new_password_confirm)
-            password_digest = BCrypt::Password.create(new_password)
-            
-            db.execute("UPDATE users SET pwdigest = ? WHERE id=?",password_digest,session[:id])
-            
+    #
+    # takes topic_id and user_id and return an array of variables
+    #
+    # @param [integer] topic_id the id of what page we are on
+    # @param [integer/nil] user_id session[:id] of current user
+    #
+    # @return [array] returns an array with [posts,users,current_user] where posts are every post with topic_id=topic_id, users are an array with hashes of every user in db, and current user is role_id of logged in user (or nil if not logged in) 
+    #
+    def show_posts_topic(topic_id,user_id)
+        db = connect_to_db()
+        db.results_as_hash = true
+        posts = db.execute("SELECT * FROM posts WHERE topic_id = ?",topic_id)
+        users = db.execute("SELECT * FROM users")
+        if user_id == nil
+            current_user = nil
         else
-            session[:change_pw_err] = "Fel lösen eller dina nya lösenord matchade inte"
-
+            current_user= db.execute("SELECT role_id FROM users WHERE id = ?",session[:id]).first
         end
-
-    else
-        session[:change_pw_err] = "Fel lösen eller dina nya lösenord matchade inte"
+        return [posts,users,current_user]
     end
-    return redirect("/users/#{session[:id]}")
-end
 
-def update_user_email(email)
-    db=connect_to_db()
-    db.execute("UPDATE users SET email= ? WHERE id=? ",email,session[:id])
-    session[:email]=email
-    return redirect("/users/#{session[:id]}")
-end
 
-def show_user(id)
-    db=connect_to_db()
-    db.results_as_hash = true
-    return db.execute("SELECT * FROM posts WHERE user_id = ?",id)
+    #
+    # takes the id of a post and returns an array of variables taken from db
+    #
+    # @param [integer] id the id of a post
+    #
+    # @return [array] returns [post,user,tags] where post is an array of hashes with information about the post with id=id, user is an array with hashes with information about the user who made the post and tags is an array with every tag the post has.
+    #
+    def show_post(id)
+        db = connect_to_db()
+        db.results_as_hash = true
+        post = db.execute("SELECT * FROM posts WHERE id = ?",id).first
+        user = db.execute("SELECT * FROM posts INNER JOIN users ON users.id = posts.user_id WHERE posts.id = ? ",id).first
+        tags = db.execute("SELECT * FROM tags INNER JOIN posts_tags_rel ON tags.id = posts_tags_rel.tags_id WHERE posts_tags_rel.posts_id = ?",id)
+        return [post,user,tags]
+    end
+
+    #
+    # Takes information about a post and creates it, also checks if a file is attatched, saves it and saves a path to it that will be in the database, if a file is not attatched then nil will be placed in place of a filepath.
+    #
+    # @param [integer] user_id the id of logged in user
+    # @param [string] title the title of new post
+    # @param [string] body the body text of new post
+    # @param [integer] topic the topic the new post will be in
+    # @param [nil/array<Hash>] file either nil (no file) or an array with hashes with information of the attatched file
+    #
+    # @return [nil] nothing
+    #
+    def create_post(user_id,title,body,topic,file)
+        if file && file[:filename]
+            path = File.join("./public/img/",file[:filename])
+            path_for_db = File.join("img/",file[:filename])
+            tempfile=file[:tempfile]
+            File.open(path, 'wb') do |f|
+                f.write(tempfile.read)
+            end
+        else
+            path_for_db = nil
+        end
+        db = connect_to_db()
+        db.execute("INSERT INTO posts (title, body, user_id, topic_id,img_path) VALUES (?, ?, ?, ?,?)",title,body,user_id,topic,path_for_db)
+        return nil
+    end
+
+    #
+    # takes the id of a post, new title and new bodytext and updates the title and body with the new ones.
+    #
+    # @param [integer] post_id The id of a post
+    # @param [string] title the new title to post
+    # @param [string] body the new body to post
+    #
+    # @return [nil] nothing
+    #
+    def update_post(post_id,title,body)
+        db=connect_to_db()
+        db.execute("UPDATE posts SET (title,body) = (?,?) WHERE id=? ",title,body,post_id)
+    end
+
+    #
+    # takes the id of a post and a tag, then checks if the tag already exists in database and adds it to database if not. After that it checks if the post already has the tag, if true then it removes tag from post in relation table, if false the it adds a relation between tag and post in ralations table.
+    #
+    # @param [integer] post_id the id of a post
+    # @param [string] post_tag A tag, new or preexisting
+    #
+    # @return [nil] nothing
+    #
+    def add_remove_tags(post_id,post_tag)
+        db=connect_to_db()
+        db.results_as_hash=true
+        tags = db.execute("SELECt * FROM tags")
+        #kolla om given tag redan finns i tags table och lägg då till den
+        @tag_exists = false
+        tags.each do |tag|
+            p tag["name"]
+            if tag["name"]==post_tag
+                @tag_exists=true
+            end
+        end
+        if @tag_exists == false 
+            db.execute("INSERT INTO tags (name) VALUES (?)",post_tag)
+        end
+        # kolla om taggen finns på post:en eller inte
+        post_tag_id = db.execute("SELECT id FROM tags WHERE name=?",post_tag).first["id"] # name är unikt så det finns bara en av alla tags
+        if db.execute("SELECT * FROM posts_tags_rel WHERE (posts_id,tags_id) = (?,?)",post_id,post_tag_id).first == nil
+            db.execute("INSERT INTO posts_tags_rel (posts_id,tags_id) VALUES (?,?)",post_id,post_tag_id)
+        else #finns det redan taggen på posten så tas relationen bort från posts_tags_rel
+            db.execute("DELETE FROM posts_tags_rel WHERE (posts_id,tags_id) = (?,?)",post_id,post_tag_id)
+        end
+        return
+    end
+
+    def delete_post(post_id,user_id)
+        db = connect_to_db()
+        post_user_id = db.execute("SELECT user_id FROM posts where id = ?",post_id)
+        path = db.execute("SELECT img_path FROM posts where id = ?", post_id).first
+        p path
+        if post_user_id != []
+            user_role=db.execute("SELECT role_id FROM users where id=?",session[:id]).first
+            if user_id==post_user_id[0][0] || user_role[0] == 1
+                db.execute("DELETE FROM posts WHERE id = ?",post_id)
+                db.execute("DELETE FROM posts_tags_rel WHERE posts_id = ?",post_id)
+                if path[0]!=nil
+                    File.delete("./public/#{path[0]}") if File.exist?("./public/#{path[0]}")
+                end
+            end      
+        end
+        return redirect("/users/#{user_id}")
+    end
+
+    #
+    # Takes a name and checks if name already exists in database and retruns a boolean
+    #
+    # @param [string] name Any name you want to check
+    #
+    # @return [boolean] Retruns true/false if username exists/does not exist
+    #
+    def username_exist?(name)
+        db = connect_to_db()
+        db.results_as_hash = true
+        usernames = db.execute("SELECT name FROM users")
+        usernames.each do |user|
+            if user["name"]==name
+                return true
+            end
+        end
+        return false
+    end
+
+    #
+    # takes a sting and checks if it is empty or not
+    #
+    # @param [sting] string given string of any size
+    #
+    # @return [boolean] retruns true if string is empty and false if not
+    #
+    def empty_string?(string)
+        return string == ""
+    end
+
+    #
+    # takes the id of a user and checks in role_id == 1, returns result
+    #
+    # @param [integer] id the id of a user
+    #
+    # @return [boolean] retuns true or false depending on role_id==1
+    #
+    def id_admin?(id)
+        db = connect_to_db()
+        return 1==db.execute("SELECT role_id FROM users where id=?",id).first
+    end
+
+    #
+    # takes the id of a post and returns the id of the user who posted it
+    #
+    # @param [integer] post_id the id of a post
+    #
+    # @return [integer] the id of the user who made the post
+    #
+    def user_id_by_post_id(post_id)
+        db = connect_to_db
+        return db.execute("SELECT FROM posts user_id WHERE id = ?",post_id).first
+    end
+
+    #
+    # takes username, email, password and password_confirm and checks if password==password_confrim. If true, make a new user in the database and regattempt=nil. If false, iterate regattempt. Returns regattempt
+    #
+    # @param [string] username username of new user
+    # @param [string] email email of new user
+    # @param [string] password password of new user
+    # @param [string] password_confirm used to check if user wrote the same password twice
+    #
+    # @return [integer/nil] returns regattempt which is either 1 or nil
+    #
+    def new_user(username,email,password,password_confirm)    
+    regattempt=0
+            if (password == password_confirm)
+        password_digest = BCrypt::Password.create(password)
+        db = connect_to_db()
+        db.execute("INSERT INTO users (name,role_id,pwdigest,email) VALUES (?,?,?,?)",username,0,password_digest,email)
+        regattempt = nil
+        return regattempt
+        else
+        regattempt +=1
+        return regattempt
+        end
+    end
+
+    #
+    # selects and retruns the information of a user where username=username
+    #
+    # @param [string] username the username of wanted user
+    #
+    # @return [array<Hash>] the information of a user in the form of an array of hashes
+    #
+    def select_user_by_name(username)
+        db = connect_to_db()
+        db.results_as_hash = true
+        return db.execute("SELECT * FROM users WHERE name = ?",username).first
+    end
+
+    #
+    # takes a given password and hash of a user to check if the password is the users
+    #
+    # @param [string] password A given password in the form of a string
+    # @param [array<Hash>] user An array of hashes related to a given 
+    #
+    # @return [boolean] true or false depending on wheter or not given password is the same as user["pwdigest"]
+    #
+    def correct_password_to_user(password,user)
+        return BCrypt::Password.new(password) == user["pwdigest"]
+    end
+
+
+    #
+    # opens the database and updates a users password
+    #
+    # @param [string] new_password the new encrypted password that overwrites old encrypted password where user_id
+    # @param [integer] user_id the id of the user having his password uppdated
+    #
+    # @return [nil] nothing
+    #
+    def update_user_pw(new_password,user_id)
+        db = connect_to_db()
+        db.execute("UPDATE users SET pwdigest = ? WHERE id=?",BCrypt::Password.new(new_password),user_id)
+        retrun nil
+    end
+
+    #
+    # updates the email of user with the given id
+    #
+    # @param [string] email the given email that will overwrite the old one
+    # @param [integer] id the id of the user having its email uppdated
+    #
+    # @return [string] returns the new email
+    #
+    def update_user_email(email,id)
+        db=connect_to_db()
+        db.execute("UPDATE users SET email= ? WHERE id=? ",email,id)   
+        return email
+    end
+
+    #
+    # retruns the information of a user where id = id
+    #
+    # @param [integer] id the id of wanted user
+    #
+    # @return [array<Hash>] the information of a user in the form of an array of hashes
+    #
+    def show_user(id)
+        db=connect_to_db()
+        db.results_as_hash = true
+        return db.execute("SELECT * FROM posts WHERE user_id = ?",id)
+    end
 end
